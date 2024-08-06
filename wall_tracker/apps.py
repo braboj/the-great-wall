4053ba8da67d12f5f6cd08b531278faf8dba3fa9
Branimir Georgiev <braboj@gmail.com>
2024-08-06 14:21:17 +0300

    [#####] - Initial commit

---
+++ wall_tracker/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class WallTrackerConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'wall_tracker'
---
+++ wall_tracker/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+# Create your models here.
+class WallProfile(models.Model):
+    pass
+
+
+class WallSection(models.Model):
+    pass
