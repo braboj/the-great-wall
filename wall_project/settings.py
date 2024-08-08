b92f218ce8ac867d7f3300c6d3571be344fb27a9
Branimir Georgiev <braboj@gmail.com>
2024-08-08 12:07:59 +0300

    (#1) - Add the administration panel

--- wall_project/settings.py
+++ wall_project/settings.py
@@ -31,12 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'wall_tracker.apps.WallTrackerConfig',
+    # 'wall_tracker.apps.WallTrackerConfig',
+    'hello_world.apps.HelloWorldConfig',
 ]
 
 MIDDLEWARE = [
--- wall_project/urls.py
+++ wall_project/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 # from django.contrib import admin
 from django.urls import path
+from hello_world import views
+from django.contrib import admin
 
 urlpatterns = [
-    #    path('admin/', admin.site.urls),
+       path('admin/', admin.site.urls),
+    path(r'', views.home, name='home'),
 ]
