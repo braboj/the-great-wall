0443e73eceb8f70209da9e5ab4f64080d5f9f852
Branimir Georgiev <braboj@gmail.com>
2024-08-11 12:45:33 +0300

    [#####] - Tag 0.0.2.0

---
+++ setup.py
@@ -0,0 +1,11 @@
+LONG_DESCRIPTION = """
+The wall is a project that provides an easy-to-use and easy-to-extend
+framework to build the Grate Wall to defend against the White Walkers.
+""".strip()
+
+SHORT_DESCRIPTION = """
+An application to build the wall using REST API.
+""".strip()
+
+VERSION = '0.0.2.0'
+URL = 'https://github.com/braboj/the-great-wall'
