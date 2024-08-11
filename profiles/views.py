d1b45722a66189d7cfddaecdf0ad5d39ce6b381d
Branimir Georgiev <braboj@gmail.com>
2024-08-11 12:07:00 +0300

    (#8) - Add a /profiles/log endpoint to access the log file

--- profiles/views.py
+++ profiles/views.py
@@ -18,88 +18,139 @@ def index(request):
 @api_view(http_method_names=["GET"])
 def get_overall_overview(request):
 
+    # Get the app
     app = apps.get_app_config("profiles")
+
+    # Get the number of teams
     num_teams = app.config.num_teams
 
     try:
+        # Build a wall with the given number of teams and days
         app.manager.build(num_teams=num_teams, days=30)
 
+    # Something went wrong
     except Exception as e:
-        return JsonResponse({'error': str(e)})
+        return HttpResponse(status=404, content=str(e))
 
+    # Everything went well
     else:
 
+        # Prepare the data
         data = {
             'day': None,
             'cost': app.manager.get_cost()
         }
 
+        # Return the data
         return JsonResponse(data)
 
 
 @api_view(http_method_names=["GET"])
 def get_day_overview(request, day_id):
 
+    # Get the app
     app = apps.get_app_config("profiles")
+
+    # Get the number of teams
     num_teams = app.config.num_teams
 
     try:
+        # Build a wall with the given number of teams and days
         app.manager.build(num_teams=num_teams, days=day_id)
 
+    # Something went wrong
     except Exception as e:
-        return JsonResponse({'error': str(e)})
+        return HttpResponse(status=404, content=str(e))
 
+    # Everything went well
     else:
+
+        # Prepare the data
         data = {
             'day': day_id,
             'cost': app.manager.get_cost()
         }
 
+        # Return the data
         return JsonResponse(data)
 
 
 @api_view(http_method_names=["GET"])
 def get_profile_overview(request, profile_id, day_id):
 
+    # Get the app
     app = apps.get_app_config("profiles")
+
+    # Get the number of teams
     num_teams = app.config.num_teams
 
     try:
+        # Build a wall with the given number of teams and days
         app.manager.build(num_teams=num_teams, days=day_id)
+
+        # Get the profile with the given ID
         profile = app.manager.get_profile(profile_id - 1)
 
+    # Something went wrong
     except Exception as e:
-        return JsonResponse({'error': str(e)})
+        return HttpResponse(status=404, content=str(e))
 
+    # Everything went well
     else:
+
+        # Prepare the data
         data = {
             'day': day_id,
             'cost': profile.get_cost()
         }
 
+        # Return the data
         return JsonResponse(data)
 
 
 @api_view(http_method_names=["GET"])
 def get_day_data(request, profile_id, day_id):
+
+    # Get the app
     app = apps.get_app_config("profiles")
+
+    # Get the number of teams
     num_teams = app.config.num_teams
 
     try:
+        # Build a wall with the given number of teams and days
         app.manager.build(num_teams=num_teams, days=day_id)
+
+        # Get the profile with the given ID
         profile = app.manager.get_profile(profile_id - 1)
 
+    # Something went wrong
     except Exception as e:
-        return JsonResponse({'error': str(e)})
+        return HttpResponse(status=404, content=str(e))
 
+    # Everything went well
     else:
 
+        # Prepare the data
         data = {
             'day': day_id,
             'ice': profile.get_ice()
         }
 
+        # Return the data
         return JsonResponse(data)
+
+
+def get_logs(request):
+
+    # Get the app
+    app = apps.get_app_config("profiles")
+
+    # Get the logs from the manager
+    logs = app.manager.get_logs()
+
+    # Return the logs
+    return JsonResponse(logs)
 
 
 @api_view(http_method_names=["POST", "GET"])
@@ -108,16 +159,35 @@ def handle_config(request):
     # Get the app
     app = apps.get_app_config("profiles")
 
-    try:
-        if request.method == "GET":
-            return JsonResponse(app.config.get_params())
+    # GET request
+    if request.method == "GET":
 
+        try:
+            # Get the configuration data
+            data = app.config.get_params()
+
+        # Something went wrong
+        except Exception as e:
+            return HttpResponse(status=500, content=str(e))
+
+        # Everything went well
         else:
-            # Set the parameters
+            return JsonResponse(data)
+
+    # POST request
+    elif request.method == "POST":
+
+        try:
+            # Set the new configuration data
             app.config.set_params(request.data)
 
-            # Return the response
-            return JsonResponse({'status': 'success'})
+            # Get the new configuration data
+            data = app.config.get_params()
 
-    except Exception as e:
-        return JsonResponse({'error': str(e)})
+        # Something went wrong
+        except Exception as e:
+            return HttpResponse(status=400, content=str(e))
+
+        # Everything went well
+        else:
+            return JsonResponse(data)
